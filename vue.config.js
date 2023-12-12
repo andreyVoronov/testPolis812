c3e76fe8942fb5485243606ea08e150efbc12958
Andrey <andrey91.workstation@icloud.com>
2023-12-12 21:06:43 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/polis812-test/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/testPolis812/' : '/',
   };
